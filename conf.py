af01b1b2f5327d759a59b0ee1bad9f47e614c640
mrzhenggang <mrzhenggang@foxmail.com>
2018-08-14 00:15:21 +0800

    change theme

--- conf.py
+++ conf.py
@@ -79,7 +79,7 @@ pygments_style = 'sphinx'
 #
 # html_theme = 'alabaster'
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinxdoc'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
